43142e9a5558f36329f72c9a56f0fb38425b39b2
madmaxWMFU <yasmaks@gmail.com>
2019-03-14 08:44:01 +0200

    fix

--- js/app.js
+++ js/app.js
@@ -7,14 +7,20 @@
 // })
 
 
-fetch('http://gateway.marvel.com/v1/public/characters?ts=1&apikey=0cc0bbc55224cb8f34ec109ab692c2cb&hash=4e7f342316da627d9a966768f39d0702')
-.then(function(response) {
-  console.log(response.json());
-})
 
-var arr = []
-while(arr.length < 9){
-    var r = Math.floor((Math.random() * 1491) + 1);
-    if(arr.indexOf(r) === -1) arr.push(r);
-}
-console.log(arr);
+// var arr = []
+// while(arr.length < 9){
+//     var r = Math.floor((Math.random() * 1491) + 1);
+// 	if(arr.indexOf(r) === -1)
+// 		fetch('http://gateway.marvel.com/v1/public/characters?limit=1&offset='+r+'&apikey=0cc0bbc55224cb8f34ec109ab692c2cb&hash=4e7f342316da627d9a966768f39d0702')
+// 		.then(function(response) {
+// 		  return response.json();
+// 		})
+// 		.then(function(data) {
+// 			console.log(data.data.results)
+// 			arr.push(data.data.results[0])
+// 		})
+// }
+// console.log(arr);
+
+
